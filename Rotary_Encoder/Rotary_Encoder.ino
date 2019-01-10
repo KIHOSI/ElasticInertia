38a34fad211a3f4655bd7525dadb377806b21cb3
KIHOSI <amie12349@gmail.com>
2019-01-10 15:45:36 +0800

    add encoder script

---
+++ Rotary_Encoder/Rotary_Encoder.ino
@@ -0,0 +1,58 @@
+int PinA = 6, PinB = 7;   // pushbutton connected to digital pin 7
+int A = 0, B = 0;     // variable to store the read value
+int recounter = 0;
+int reA, reB, rePA, rePB;
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(PinA, INPUT);
+  pinMode(PinB, INPUT);
+  rePA = digitalRead(PinA);
+  rePB = digitalRead(PinB);
+}
+
+void loop()
+{
+  reA = digitalRead(PinA); // Reads the "current" state of the outputA
+  reB = digitalRead(PinB);
+  
+  
+  // If the previous and the current state of the outputA are different, that means a Pulse has occured
+  if (reA != rePA || reB != rePB) {
+    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+    if(reA != rePA && reB != rePB){ // rotate too fast, miss 1 change
+      recounter += 2;
+    }
+
+    
+    else if (reA == rePB) {
+      recounter ++;
+    } else {
+      recounter --;
+    }
+    Serial.print("A: ");
+    Serial.print(reA);
+    Serial.print(", ");
+    Serial.print("B: ");
+    Serial.println(reB);
+    Serial.print("Position: ");
+    Serial.println(recounter);
+  }
+  
+  
+  rePA = reA; // Updates the previous state of the outputA with the current state
+  rePB = reB;
+
+  /*
+    A = digitalRead(PinA);   // read the input pin
+    Serial.print("A: ");
+    Serial.print(A);
+    Serial.print(", ");
+    B = digitalRead(PinB);   // read the input pin
+    Serial.print("B: ");
+    Serial.println(B);
+  */
+
+  //Serial.print("RE counter: ");
+  //Serial.println(recounter);
+}
